f15d40ef64f5510d5dfb9b733022cdcd4839471e
zxh-im <z@zxh.im>
2019-10-22 16:59:18 +0800

    init

---
+++ client/src/test/java/im/zxh/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package im.zxh.client;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka-service/src/test/java/im/zxh/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package im.zxh.eurekaservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
