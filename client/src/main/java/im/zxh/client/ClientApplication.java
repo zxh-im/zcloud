54378880805847a943a78a92ee27a850657815ff
zhao.xiaohu <z@zxh.im>
2019-10-23 22:43:45 +0800

    spring-cloud-starter-hystrix

--- client/src/main/java/im/zxh/client/ClientApplication.java
+++ client/src/main/java/im/zxh/client/ClientApplication.java
@@ -3,8 +3,11 @@ package im.zxh.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableDiscoveryClient 使用其他注册中心时用
 @EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class ClientApplication {
 
